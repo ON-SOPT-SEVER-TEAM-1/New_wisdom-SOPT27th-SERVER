c0916f5e7af90ff92f49b1a421d2de9861384f11
NewWisdom <wlgp2500@gmail.com>
2020-11-10 16:06:53 +0900

    4th-Seminar

---
+++ 4th-seminar/models/user.js
@@ -0,0 +1,28 @@
+const { sequelize } = require(".");
+
+module.exports = (sequelize, DataTypes) =>{
+    return sequelize.define('User',{
+        // 모델의 attributes (column)
+        email : {
+            type : DataTypes.STRING(30),
+            unique : true,
+            allowNull : false,
+        },
+        userName :{
+            type: DataTypes.STRING(20),
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING(200),
+            allowNull: false,
+        },
+        salt: {
+            type: DataTypes.STRING(200),
+            allowNull: false,
+        },
+    },{
+        // 모델의 옵션
+        freezeTableName : true,
+        timestamps : false,
+    })
+}
