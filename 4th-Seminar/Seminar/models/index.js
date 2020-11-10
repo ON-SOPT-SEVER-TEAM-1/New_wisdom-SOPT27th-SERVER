e079e1961935b838ff6c83956d2735477f15c9c0
NewWisdom <wlgp2500@gmail.com>
2020-11-10 16:13:03 +0900

    4th-Seminar

--- 4th-Seminar/Seminar/models/index.js
+++ 4th-Seminar/Seminar/models/index.js
@@ -1,4 +1,4 @@
-const Sequelize = require('sequelize');
+const Sequelize = require('sequelize/types');
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config.json')[env];
 const db = {};
